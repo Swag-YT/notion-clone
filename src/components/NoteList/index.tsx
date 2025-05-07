7cfb22a339433d98eb14e6598b1c9f8c97f64f0f
Swag-YT <yumita.tomoki@idh-net.sakura.ne.jp>
2025-05-07 12:39:49 +0900

    子供のノートを入れ子で表示

--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -39,10 +39,13 @@ export function NoteList({ layer = 0, parentId }: NoteListProps) {
       >
         ページがありません
       </p>
-      {notes.map((note) => {
+      {notes.
+      filter((note)=>note.parent_document == parentId)
+      .map((note) => {
         return (
           <div key={note.id}>
             <NoteItem note={note} layer={layer} onExpand={(e:React.MouseEvent) => fetchChildren(e,note)} onCreate={(e) => createChild(e,note.id)}/>
+            <NoteList layer={layer + 1} parentId={note.id}/>
           </div>
         );
       })}
