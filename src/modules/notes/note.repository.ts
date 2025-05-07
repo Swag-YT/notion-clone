eb085d25d06e34e63a8265a731d8ef00b18c05ee
Swag-YT <yumita.tomoki@idh-net.sakura.ne.jp>
2025-05-07 11:32:35 +0900

    ノートの情報を画面に表示

--- src/modules/notes/note.repository.ts
+++ src/modules/notes/note.repository.ts
@@ -19,7 +19,7 @@ export const noteRepository = {
     if(error !=null) throw new Error(error.message);
     return data;
   },
-  async find(userId:string,parentDocumentId:number) {
+  async find(userId:string,parentDocumentId?:number) {
     const query = supabase.from('notes')
     .select()
     .eq('user_id',userId)
